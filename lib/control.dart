f21d96bda234a8c0f68d2daacfca3e02de9a4659
Anant Kanchan <anant4265@gmail.com>
2021-02-08 20:00:31 +0530

    Login and Control files added,routes created

---
+++ lib/control.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Control extends StatefulWidget {
+  @override
+  _ControlState createState() => _ControlState();
+}
+
+class _ControlState extends State<Control> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'control.dart';
+import 'login.dart';
+
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Login(),
+    routes: {
+
+      '/control':(context)=>Control(),
+      // '/homePage':(context)=>HomePage()
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.green,
-      ),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.green,
+//       ),
+//     );
+//   }
+// }
+
+
 
 
 
