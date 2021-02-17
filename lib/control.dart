20e80138e4de066dabd0ccdc49b9504525576775
Anant Kanchan <anant4265@gmail.com>
2021-02-17 10:42:30 +0530

    code formatted using L

--- lib/control.dart
+++ lib/control.dart
@@ -9,51 +9,37 @@ class _ControlState extends State<Control> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text(
-                "Home page",
-              ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Home page",
             ),
-            backgroundColor: Colors.yellow,
-            body:
-
-                  Text('Text assigned',
-                      style: TextStyle(
-                          // your text
-                          fontSize: 50.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white)
-                  ),
-                  // FlatButton(
-                  //   child: Text('Control center'),
-                  //   color: Colors.blue,
-                  //   onPressed: () {
-                  //     Navigator.pushNamed(context, '/login');
-                  //   },
-                  // ),
-                  // ],
-
-                bottomNavigationBar : BottomNavigationBar(
-                        items: const <BottomNavigationBarItem>[
-                          BottomNavigationBarItem(
-                            icon: Icon(Icons.home),
-                            label: 'Home',
-                          ),
-                          BottomNavigationBarItem(
-                            icon: Icon(Icons.business),
-                            label: 'Business',
-                          ),
-                          BottomNavigationBarItem(
-                            icon: Icon(Icons.school),
-                            label: 'School',
-                          ),
-                        ],
-                      )
-                  ),
-
-
-
+          ),
+          backgroundColor: Colors.yellow,
+          body: Text('Text assigned',
+              style: TextStyle(
+                  // your text
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white)
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business),
+                label: 'Business',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                label: 'School',
+              ),
+            ],
+          )
+      ),
     );
   }
 }
