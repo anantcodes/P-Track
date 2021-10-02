1e304d2a4ae861fbce88001754e08a38ce099dab
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2021-10-02 11:38:44 +0530

    splash page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+
 import 'control.dart';
 import 'login.dart';
+import 'splash.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Login(),
-    routes: {
-      '/control': (context) => Control(),
-      '/login': (context) => Login()
-    },
-  )
+  runApp(
+    MaterialApp(
+      home: Splash(),
+      routes: {
+        '/control': (context) => Control(),
+        '/login': (context) => Login()
+      },
+    ),
   );
 }
-
