bdb83e66379ff0bed1fe1b7b6f9e7ce635ef1378
kishorkc <kishor8007kc@gmail.com>
2021-10-06 11:11:22 +0545

    set as default main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
+
 import 'control.dart';
 import 'login.dart';
 import 'splash.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       home: Splash(),
       routes: {
         '/control': (context) => Control(),
         '/login': (context) => Login()
       },
-    );
-  }
+    ),
+  );
 }
