47419f4ef0d5cea3ceaa26b6b56e8e17db018c72
purushottam212 <purushottamvaidya212@gmail.com>
2021-10-07 12:35:30 +0530

    login page : fixed botton overflow

--- lib/login.dart
+++ lib/login.dart
@@ -12,7 +12,7 @@ class _LoginState extends State<Login> {
   TextEditingController passwordController =
       new TextEditingController(); //To store password
 
- String _chosenValue;
+  String _chosenValue;
 
   @override
   Widget build(BuildContext context) {
@@ -22,93 +22,97 @@ class _LoginState extends State<Login> {
         appBar: AppBar(
           title: Text("Login page"),
         ),
-        body: Center(
-          child: Container(
-              color: Colors.white,
-              child: Padding(
-                  padding: const EdgeInsets.all(36.0),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        SizedBox(height: 45.0),
-                        TextField(
-                          controller: emailController, //email
-                          decoration:
-                              InputDecoration(labelText: 'Enter your email'),
-                        ),
-                        SizedBox(height: 25.0),
-                        TextField(
-                          controller: passwordController, //password
-                          decoration:
-                              InputDecoration(labelText: 'Enter your password'),
-                          obscureText: true, //text hiding
-                        ),
-                        SizedBox(height: 45.0),
-                        DropdownButton<String>(
-                          focusColor:Colors.white,
-                          value: _chosenValue,
-                          //elevation: 5,
-                          style: TextStyle(color: Colors.white),
-                          iconEnabledColor:Colors.black,
-                          items: <String>[
-                            'Home',
-                            'Business',
-                            'School',
-                          ].map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value,style:TextStyle(color:Colors.black),),
-                            );
-                          }).toList(),
-                          hint:Text(
-                            "Please choose a track",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+                height: MediaQuery.of(context).size.height,
+                color: Colors.white,
+                child: Padding(
+                    padding: const EdgeInsets.all(36.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SizedBox(height: 45.0),
+                          TextField(
+                            controller: emailController, //email
+                            decoration:
+                                InputDecoration(labelText: 'Enter your email'),
                           ),
-                          onChanged: (String value) {
-                            setState(() {
-                              _chosenValue = value;
-                            });
-                          },
-                        ),
-                        SizedBox(height: 45.0),
-                        TextButton(
-                          style: TextButton.styleFrom(
-                            primary: Colors.white,
-                            backgroundColor: Colors.blue,
-                            shadowColor: Colors.grey,
-                            elevation: 5.0,
+                          SizedBox(height: 25.0),
+                          TextField(
+                            controller: passwordController, //password
+                            decoration: InputDecoration(
+                                labelText: 'Enter your password'),
+                            obscureText: true, //text hiding
                           ),
-                          onPressed: () {
-                            if (emailController.text == 'anant1234@gmail.com' &&
-                                passwordController.text ==
-                                    'anant') // email and password static
-                            {
-                              Navigator.pushNamed(
-                                  context, '/control'); //push to next route
-                              Fluttertoast.showToast(
-                                msg: 'Login successfull',
-                                textColor: Colors.red,
-                                backgroundColor: Colors.grey,
-                                gravity: ToastGravity.BOTTOM,
+                          SizedBox(height: 45.0),
+                          DropdownButton<String>(
+                            focusColor: Colors.white,
+                            value: _chosenValue,
+                            //elevation: 5,
+                            style: TextStyle(color: Colors.white),
+                            iconEnabledColor: Colors.black,
+                            items: <String>[
+                              'Home',
+                              'Business',
+                              'School',
+                            ].map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(
+                                  value,
+                                  style: TextStyle(color: Colors.black),
+                                ),
                               );
-                            } else {
-                              Fluttertoast.showToast(
-                                  msg: 'Login unsuccessfull',
+                            }).toList(),
+                            hint: Text(
+                              "Please choose a track",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            onChanged: (String value) {
+                              setState(() {
+                                _chosenValue = value;
+                              });
+                            },
+                          ),
+                          SizedBox(height: 45.0),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              primary: Colors.white,
+                              backgroundColor: Colors.blue,
+                              shadowColor: Colors.grey,
+                              elevation: 5.0,
+                            ),
+                            onPressed: () {
+                              if (emailController.text ==
+                                      'anant1234@gmail.com' &&
+                                  passwordController.text ==
+                                      'anant') // email and password static
+                              {
+                                Navigator.pushNamed(
+                                    context, '/control'); //push to next route
+                                Fluttertoast.showToast(
+                                  msg: 'Login successfull',
                                   textColor: Colors.red,
                                   backgroundColor: Colors.grey,
-                                  toastLength: Toast.LENGTH_SHORT,
-                                  gravity: ToastGravity.BOTTOM);
-                            }
-                          },
-                          child: Text('Login'),
-                        )
-                      ]
-                  )
-              )
+                                  gravity: ToastGravity.BOTTOM,
+                                );
+                              } else {
+                                Fluttertoast.showToast(
+                                    msg: 'Login unsuccessfull',
+                                    textColor: Colors.red,
+                                    backgroundColor: Colors.grey,
+                                    toastLength: Toast.LENGTH_SHORT,
+                                    gravity: ToastGravity.BOTTOM);
+                              }
+                            },
+                            child: Text('Login'),
+                          )
+                        ]))),
           ),
         ),
       ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:p_track/main.dart';
+import 'package:p_track/splash.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Splash());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
