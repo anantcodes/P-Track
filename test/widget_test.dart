f220b1aaf47b6082e7bb575c53a5970c6a026118
Ashish Chaubey <ashishchaubey866@gmail.com>
2021-10-07 14:35:47 +0530

    initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:p_track/main.dart';
 import 'package:p_track/splash.dart';
 
 void main() {
