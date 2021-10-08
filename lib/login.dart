4daca9a150f53ec3604c87e08f3841b58e3c7fad
lit-lalit <75823395+lit-lalit@users.noreply.github.com>
2021-10-09 02:37:00 +0530

    Improved UI of login screen

--- lib/login.dart
+++ lib/login.dart
@@ -35,58 +35,98 @@ class _LoginState extends State<Login> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget>[
                           SizedBox(height: 45.0),
-                          TextField(
-                            controller: emailController, //email
-                            decoration:
-                                InputDecoration(labelText: 'Enter your email'),
-                          ),
-                          SizedBox(height: 25.0),
-                          TextField(
-                            controller: passwordController, //password
-                            decoration: InputDecoration(
-                                labelText: 'Enter your password'),
-                            obscureText: true, //text hiding
-                          ),
-                          SizedBox(height: 45.0),
-                          DropdownButton<String>(
-                            focusColor: Colors.white,
-                            value: _chosenValue,
-                            //elevation: 5,
-                            style: TextStyle(color: Colors.white),
-                            iconEnabledColor: Colors.black,
-                            items: <String>[
-                              'Home',
-                              'Business',
-                              'School',
-                            ].map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(
-                                  value,
-                                  style: TextStyle(color: Colors.black),
-                                ),
-                              );
-                            }).toList(),
-                            hint: Text(
-                              "Please choose a track",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500),
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color:Colors.blue),
+                              borderRadius: BorderRadius.only(
+                                topLeft:Radius.circular(10),
+                                topRight: Radius.circular(10),
+                              ),
                             ),
-                            onChanged: (String value) {
-                              setState(() {
-                                _chosenValue = value;
-                              });
-                            },
+                            child: TextField(
+                              controller: emailController, //email
+                              decoration:
+                                  InputDecoration(labelText: 'Enter your email',
+                                  border: InputBorder.none,
+                                    prefixIcon: Icon(Icons.email),
+                                  ),
+                            ),
                           ),
-                          SizedBox(height: 45.0),
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color:Colors.blue),
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft:Radius.circular(10),
+                                  bottomRight: Radius.circular(10),
+                                ),
+
+                          ),
+                            child: TextField(
+                              controller: passwordController, //password
+                              decoration: InputDecoration(
+                                  labelText: 'Enter your password',
+                                border: InputBorder.none,
+                                prefixIcon: Icon(Icons.password_outlined),
+
+                              ),
+                              obscureText: true, //text hiding
+                            ),
+                          ),
+                          SizedBox(height: 35.0),
+                          Container(
+                            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                border: Border.all(color: Colors.blue)),
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton<String>(
+                                focusColor: Colors.white,
+                                value: _chosenValue,
+                                elevation: 15,
+                                style: TextStyle(color: Colors.white),
+                                iconEnabledColor: Colors.blue,
+                                items: <String>[
+                                  'Home',
+                                  'Business',
+                                  'School',
+                                ].map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(
+                                      value,
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                  );
+                                }).toList(),
+                                hint: Text(
+                                  "Please choose a track",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                onChanged: (String value) {
+                                  setState(() {
+                                    _chosenValue = value;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 35.0),
                           TextButton(
-                            style: TextButton.styleFrom(
-                              primary: Colors.white,
-                              backgroundColor: Colors.blue,
-                              shadowColor: Colors.grey,
-                              elevation: 5.0,
+                            style: ButtonStyle(
+                                side: MaterialStateProperty.all(
+                                    BorderSide(width: 1, color: Colors.blue)),
+                                foregroundColor: MaterialStateProperty.all(Colors.blue),
+                                padding: MaterialStateProperty.all(
+                                    EdgeInsets.symmetric(vertical: 10, horizontal: 50)),
+                                textStyle:MaterialStateProperty.all(TextStyle(fontSize: 30,)),
+                              elevation: MaterialStateProperty.all(10),
+                              backgroundColor: MaterialStateProperty.all(Colors.blue),
+                              shape:  MaterialStateProperty.all(RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(7.0),
+                              ),)
                             ),
                             onPressed: () {
                               if (emailController.text ==
@@ -116,7 +156,12 @@ class _LoginState extends State<Login> {
                                     gravity: ToastGravity.BOTTOM);
                               }
                             },
-                            child: Text('Login'),
+                            child: Text('Login',
+                            style: TextStyle(
+                              fontSize: 21,
+                                color: Colors.white
+                            ),
+                            ),
                           )
                         ]
                     )
